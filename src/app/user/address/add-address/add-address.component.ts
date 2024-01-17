56328457f008ba7df001ac0c58776be351e0f410
Vishakh A <vishakhaarayalil@gmail.com>
2024-01-17 22:54:23 +0530

    add-address

--- src/app/user/address/add-address/add-address.component.ts
+++ src/app/user/address/add-address/add-address.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-address',
   templateUrl: './add-address.component.html',
   styleUrls: ['./add-address.component.css']
 })
-export class AddAddressComponent {
+export class AddAddressComponent implements OnInit{
 
+  addressForm: FormGroup;
+  states:any=null;
+  district:any=null;
+  location=[
+    {
+      country:'india',
+      state:[{stateName:'kerala',
+        dist:['Kannur', 'calicut','Malapuram']},
+      {stateName:'TamilNadu',
+    dist:['chennai','coimbatore']}]
+    },
+    {
+      country:'America',
+      state:[{stateName:'california',
+        dist:['moodadi', 'Meenchatha','Mattannur']}]
+    },
+    
+  ]
+
+constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.addressForm= this.fb.group({
+      personName :[null,Validators.required],
+      personMobile:[null,Validators.required],
+      personAddres:[null,Validators.required],
+      personArea:[null,Validators.required],
+      personLandmark:[null,Validators.required],
+      personCity:[null,Validators.required],
+      personPin:[null,Validators.required],
+      personCountry:[null,Validators.required],
+      personState:[null,Validators.required],
+      personDistrict:[null,Validators.required],
+
+      
+    })
+
+    this.addressForm.get('personCountry')?.valueChanges.subscribe(value=>{this.onCountryChange(value)})
+    this.addressForm.get('personState')?.valueChanges.subscribe(value=>{this.onStateChange(value)})
+
+
+  }
+  onCountryChange(value:any){
+    
+    
+    const [data] = this.location.filter(item=>item.country==value)
+    this.states=data;
+    console.log(this.states);
+    
+  }
+  onStateChange(value:any){
+    
+    const [data] =this.states.state.filter((item:any)=>item.stateName==value)
+    this.district=data.dist;
+    console.log(this.district);
+    
+  }
+  onSubmit(){
+
+  }
 }
